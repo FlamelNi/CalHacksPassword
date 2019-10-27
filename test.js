4d0b3a51339db65eca984d36a59620a8a5055f2b
Jae Min Im <jaeminim@berkeley.edu>
2019-10-26 17:21:14 -0700

    few changes

--- test.js
+++ test.js
@@ -2,27 +2,27 @@
 //this is javascript
 //test2
 
+var valid_chars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz123456789!@#$%^&*()_+-=<>?';
+
 function generate_password(len)
 {
-    return map_str(random_str(len))
+    return map_str(random_str(len));
 }
 
 function map_str(str)
 {
-    return str
+    return str;
 }
 
 function random_str(len)
 {
-    return '0000'
+    return '0000';
 }
 
 function generate_char()
 {
-    
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=<>?';
-    var charactersLength = characters.length;
-    return characters.charAt(Math.floor(Math.random() * charactersLength))
+    var charactersLength = valid_chars.length;
+    return valid_chars.charAt(Math.floor(Math.random() * charactersLength))
     
 }
 
